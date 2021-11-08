d6d5f0db9d9d3d1789eee80168ef3a72ed982b72
MHDibaei <mhdibaei@outlook.com>
2021-11-08 13:32:27 +0330

    !

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+var placeCityName = document.getElementById('city-name');
+var placeCityTemp = document.getElementById('temp');
+var placeCityWeatherDesc = document.getElementById('temp-desc');
+var placeCityDate  = document.getElementById('date-container');
+
+
+const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+function getData(city){
+    let date = new Date;
+    let cityName = `${city}`;
+    let APIKey = '13a49ece254b7f95e3e56d715eddedbe';
+    
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}&units=metric`)
+    .then(response => response.json())
+    .then(data => placer(data))
+    
+    function placer(data) {
+        var cityName = data.name;
+        var cityTemp = data.main.temp;
+        var cityWeatherDesc = data.weather[0].main;
+        var cityDateM = month[date.getMonth()];
+        var cityDateWD = weekDays[date.getDay()];
+        var cityDateD = date.getDate();
+
+        placeCityName.innerText = cityName;
+        placeCityTemp.innerText = Math.round(cityTemp);
+        placeCityWeatherDesc.innerText = cityWeatherDesc;
+        placeCityDate.innerText = `${cityDateWD}, ${cityDateM}${cityDateD}`
+    };
+};
+getData('london');
