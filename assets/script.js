568895a99c59cfe7d7e5141c27592bf51bc66464
MHDibaei <mhdibaei@outlook.com>
2021-11-15 11:12:15 +0330

    fixing some bugs

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,28 @@
 var placeCityName = document.getElementById('city-name');
 var placeCityTemp = document.getElementById('temp');
 var placeCityWeatherDesc = document.getElementById('temp-desc');
-var placeCityDate  = document.getElementById('date-container');
+var placeCityDate = document.getElementById('date-container');
+let searchValue = document.getElementById('city-input');
+let waetherCard = document.getElementById('weather-card');
 
 
 const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-function getWeatherData(city){
+function search() {
+    getWeatherData(`${searchValue.value}`);
+    waetherCard.style.display = 'flex';
+}
+
+function getWeatherData(city) {
     let date = new Date;
     let cityName = `${city}`;
     let APIKey = '13a49ece254b7f95e3e56d715eddedbe';
-    
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}&units=metric`)
-    .then(response => response.json())
-    .then(data => placer(data))
-    
+
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}&units=metric`)
+        .then(response => response.json())
+        .then(data => placer(data))
+
     function placer(data) {
         var cityName = data.name;
         var cityTemp = data.main.temp;
@@ -30,8 +37,3 @@ function getWeatherData(city){
         placeCityDate.innerText = `${cityDateWD}, ${cityDateM}${cityDateD}`
     };
 };
-
-function search(){
-    let searchValue = document.getElementById('city-input');
-    getWeatherData(`${searchValue.value}`);
-}
